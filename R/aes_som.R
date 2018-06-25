1e7cde587edd361f37e98a2c6782e227755b5bba
Felipe <lipecaso@gmail.com>
2018-06-25 16:50:19 -0300

    refactoring the code

--- R/aes_som.R
+++ R/aes_som.R
@@ -1,14 +1,12 @@
 #' aesthetics data of som object
 #
-#' @param model_som object of SOM package
+#' @param model_som object of self-organising maps (SOMs) package
 #'
-#' @param cutree.value number of division grid
+#' @param cutree_values numbers of divisions per grid
 #'
 #' @import tidyverse
 #'
 #' @return Model data that is used in plots
-#'
-#' @export
 aes_som <- function(model_som,
                     cutree_value = 0) {
   cutree <- TRUE
@@ -16,53 +14,60 @@ aes_som <- function(model_som,
   # Check if the variable is a SOMs object
   "%|K|%" (model_som)
 
-  # Wide model with IDs using key and value
+  # Creating a data frame from the SOMs object
   model_som_values <-
     data.frame(
       codes = model_som$data[[1]],
       id = (1:nrow(model_som$data[[1]])),
       unit.class = model_som$unit.classif
-    ) %>%
-    gather(var, values, -id, -unit.class)
+    )
 
-  # Create the model data of points
+  # Creating a data frame from the SOMs object of grid points
   model_som_pts <- as.data.frame(model_som$grid$pts)
-
   model_som_pts$unit.class <- c(1:nrow(model_som$grid$pts))
+  model_som_pts <-
+    left_join(model_som_pts, sum_unit_class(model_som_values), by = "unit.class")
 
-  value_sum_points <- sum_unit_class(model_som_values)
-  model_som_pts$sum <- value_sum_points$sum
+  # Set 0 to NA values
+  model_som_pts$sum <- replace_na(model_som_pts$sum, 0)
 
-  # joining the model of points  of model of values by unit.class column
+  # Verifies if number of divisions is greater than 0
   if (cutree && cutree_value > 0) {
+    # Using hierarchical clustering as a cluster analysis method
     model_som_pts$cluster <-
       cutree(hclust(dist(model_som$codes[[1]])), cutree_value)
-    model_som_result <-
-      left_join(model_som_values, model_som_pts, by = "unit.class")
-    return(model_som_result)
-  } else {
-    model_som_result <-
-      left_join(model_som_values, model_som_pts, by = "unit.class")
-    return(model_som_result)
-  }
 
+    model_result <-
+      left_join(model_som_values, model_som_pts, by = "unit.class") %>%
+      gather(var, values, -id, -unit.class, -cluster,-sum, -x, -y)
+
+    return(model_result)
+  } else {
+    model_result <-
+      left_join(model_som_values, model_som_pts, by = "unit.class") %>%
+      gather(var, values, -id, -unit.class, -sum, -x, -y)
+
+    return(model_result)
+  }
 }
+
 #' sum_unit_class
 #'
 #' Sum of each grid the neuron
 #'
-#' @param model.som.values Model of values that is generate in generate.model.result
+#' @param model_som_values Model of values that is generate in generate.model.result
 #'
 #' @return Sum of amount neurons per grid
 sum_unit_class <- function(model_som_values) {
+  # Receive the unique values of the sorted units
   value_result <-
-    data.frame(unit.class = c(1:length(unique(
-      model_som_values$unit.class
-    ))))
+    data.frame(unit.class = c(unique(model_som_values$unit.class)))
 
-  for (value in seq_along(unique(model_som_values$unit.class))) {
-    value_result$sum[value] <-
-      c(model_som_values[model_som_values$unit.class == value, "values"] %>% length())
-  }
+  # In each unique unit sapply the function that sum the numbers of row
+  value_result$sum <-
+    sapply(value_result$unit.class,
+           (function(value)
+             filter (model_som_values, unit.class == as.character(value)) %>% nrow()))
+
   return(value_result)
 }
